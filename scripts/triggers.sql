30f01c381635ad59c5034af589d3610744632ce1
BScott-c <gbrookec@byu.edu>
2022-02-26 09:58:37 -0700

    add insert triggers

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -7,3 +7,13 @@ CREATE TRIGGER set_update_timestamp_recipe
 BEFORE UPDATE ON recipe
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_update_timestamp_recipe();
+
+CREATE TRIGGER set_insert_timestamp_cookbook
+BEFORE INSERT ON cookbook
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_update_timestamp_cookbook();
+
+CREATE TRIGGER set_insert_timestamp_recipe
+BEFORE INSERT ON recipe
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_update_timestamp_recipe();
