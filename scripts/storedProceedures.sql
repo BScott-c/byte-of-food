2906a32744ec847b69275a48ab1efd6c823f329e
BScott-c <gbrookec@byu.edu>
2022-02-26 09:52:19 -0700

    update function and triggers

--- scripts/storedProceedures.sql
+++ scripts/storedProceedures.sql
@@ -1,7 +1,15 @@
-CREATE OR REPLACE FUNCTION trigger_set_update_timestamp(@rowName varchar)
+CREATE OR REPLACE FUNCTION trigger_set_update_timestamp_recipe()
 RETURNS TRIGGER AS $$
 BEGIN
-  NEW.@rowName = NOW();
+  NEW.recipeUpdatedDate = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION trigger_set_update_timestamp_cookbook()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.cookbookUpdatedDate = NOW();
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -1,9 +1,9 @@
-CREATE TRIGGER set_update_timestamp
+CREATE TRIGGER set_update_timestamp_cookbook
 BEFORE UPDATE ON cookbook
 FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_update_timestamp(cookbookUpdatedDate);
+EXECUTE PROCEDURE trigger_set_update_timestamp_cookbook();
 
-CREATE TRIGGER set_update_timestamp
+CREATE TRIGGER set_update_timestamp_recipe
 BEFORE UPDATE ON recipe
 FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_update_timestamp(recipeUpdatedDate);
+EXECUTE PROCEDURE trigger_set_update_timestamp_recipe();
