9ae98215819c864bc67868ded06161e27385193d
BScott-c <gbrookec@byu.edu>
2022-02-26 12:29:59 -0700

    update postman collection

--- scripts/CreateViews.sql
+++ scripts/CreateViews.sql
@@ -1,30 +1,30 @@
 -- Get Cookbooks for a single user  where userId = ?
-CREATE VIEW UserCookbooks AS 
+CREATE OR REPLACE VIEW UserCookbooks AS 
 SELECT userid, cookbookid, cookbookname, cookbookdescription 
 from cookbook;
 -- Get Recipes Associated with a single Cookbook  where h.cookbookid = ?
-CREATE VIEW CookbookRecipes AS 
+CREATE OR REPLACE VIEW CookbookRecipes AS 
 SELECT h.cookbookid, r.recipeid, r.recipename, r.recipedescription, r.isprivate from recipe as r 
 FULL JOIN holds as h on r.recipeid = h.recipeid;
 -- Get Recipe by recipe id where recipeid = '3'
-CREATE VIEW Recipes AS 
+CREATE OR REPLACE VIEW Recipes AS 
 SELECT recipeid, recipename, recipeinstructions 
 from recipe;
 -- Get Recipe Equipment where i.recipeid = '3'
-CREATE VIEW RecipeEquipment AS
+CREATE OR REPLACE VIEW RecipeEquipment AS
 SELECT i.recipeid, e.equipmentname, e.equipmentdescription from equipment as e 
 FULL JOIN involves as i on e.equipmentid = i.equipmentid;
 -- Get Recipe Ingredients where ing.recipeid = '3'
-CREATE VIEW RecipeIngredients AS
+CREATE OR REPLACE VIEW RecipeIngredients AS
 SELECT ing.recipeid, item.itemname, ing.ingredientamount, ing.ingredientmeasurement from ingredient as ing
 FULL JOIN item on ing.itemid = item.itemid;
 -- Get Favorited Recipes where f.userid = '2'
-CREATE VIEW FavoriteRecipes AS
+CREATE OR REPLACE VIEW FavoriteRecipes AS
 SELECT f.userid, r.recipeid, r.recipename, r.recipedescription, r.isprivate from recipe as r 
 FULL JOIN favorites as f on r.recipeid = f.recipeid;
 -- Get Admin Page
-CREATE VIEW AdminPage AS
+CREATE OR REPLACE VIEW AdminPage AS
 SELECT userid, userfirstname, userlastname, isadmin from usertable;
 -- Get User Admin Status where userid = '2'
-CREATE VIEW AdminStatus AS
+CREATE OR REPLACE VIEW AdminStatus AS
 SELECT userid, isadmin from usertable;
