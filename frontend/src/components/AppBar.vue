6931537a63da681d33a703c9644fcd62644d18c8
TingeyTime <ntingey@gmail.com>
2022-03-07 14:03:59 -0700

    add styling

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <!-- Image and text -->
-  <b-navbar variant="faded" type="light">
+  <b-navbar variant="info" type="dark">
     <b-navbar-brand href="#">
       <b-icon-journal-bookmark></b-icon-journal-bookmark>
       Byte Of Food
@@ -9,10 +9,11 @@
     <b-navbar-text>
       Create to your hearts content...
     </b-navbar-text>
-    <b-navbar-nav>
+    <b-navbar-nav class="ml-auto">
       <b-nav-item to="/">Cookbooks</b-nav-item>
       <b-nav-item to="/">Favorite Recipes</b-nav-item>
-      <b-button to="/Logout">Logout</b-button>
+      <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+      <b-button variant="danger" to="/Logout">Logout</b-button>
     </b-navbar-nav>
   </b-navbar>
 </div>
