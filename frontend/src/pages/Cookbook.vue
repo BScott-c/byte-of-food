8875208588515613f83d37b1bdee5806ecc77fa9
TingeyTime <ntingey@gmail.com>
2022-03-11 17:34:46 -0700

    fix recipes

--- frontend/src/pages/Cookbook.vue
+++ frontend/src/pages/Cookbook.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    <h1>{{this.cookbook.cookbookname}}</h1>
-    <h3>{{this.cookbook.cookbookdescription}}</h3>
-    <br /><br />
-    <button @click="goToAddRecipe()">Add Recipe</button>
-    <div v-if="loading">Loading recipes....</div>
-    <div v-else><CookbooksRecipeList v-bind:allRecipes="this.recipes"/></div>
-  </div>
+  <b-container>
+    <b-row>
+      <b-col>
+          <a href='/'>« Back to Recipes</a>
+        <b-container class="mx-auto h-50 text-center">
+        <h1>{{this.cookbook.cookbookname}}</h1>
+        <p class="description">
+          {{this.cookbook.cookbookdescription}}
+        </p>
+        </b-container>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-container class="mx-auto h-75 mb-1" align-v="center">
+        <b-button pill variant="primary" @click="goToAddRecipe()">Add Recipe</b-button>
+        </b-container>
+        <div v-if="loading">Loading recipes....</div>
+        <div v-else>
+          <CookbooksRecipeList v-bind:allRecipes="this.recipes"/>
+        </div>  
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
